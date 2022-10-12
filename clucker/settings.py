106f490b32ffe582d745365ee9187179885996ce
Matthew Klein <matthew-a-klein@github.com>
2022-10-12 11:55:49 +0100

    Add user class inherited from Django User

--- clucker/settings.py
+++ clucker/settings.py
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Usr model for authentication
+AUTH_USER_MODEL = 'microblogs.User'
