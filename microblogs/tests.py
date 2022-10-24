b088ba1572395b977b6d77e813d29c326f72267c
Matthew Klein <matthew-a-klein@github.com>
2022-10-24 11:48:57 +0100

    Add a user sign up page

--- microblogs/tests.py
+++ microblogs/tests.py
@@ -1,3 +1,52 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
 
-# Create your tests here.
+from .models import User
+
+
+class UserModelTestCase(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            '@johndoe',
+            first_name='John',
+            last_name='Doe',
+            email='johndoe@example.com',
+            password='Password123',
+            bio='My name is John Doe.',
+        )
+
+    def test_valid_user(self):
+        self._assert_user_is_valid()
+
+    def test_username_cannot_be_blank(self):
+        self.user.username = ''
+        self._assert_user_is_invalid()
+
+    def test_username_can_be_thirty_characters(self):
+        self.username = '' + 'x' * 30
+        self._assert_user_is_valid()
+
+    def test_username_can_be_thirty_characters(self):
+        self.username = ''
+        self._assert_user_is_invalid()
+
+    def test_username_must_be_unique(self):
+        User.objects.create_user('@janedoe',
+                                 first_name='Jane',
+                                 last_name='Doe',
+                                 email='janedoe@example.com',
+                                 password='Password123',
+                                 bio='My name is JaneDoe.', )
+        self.username='@janedoe'
+        self._assert_user_is_invalid();
+
+    def _assert_user_is_valid(self):
+        try:
+            self.user.full_clean()
+        except ValidationError:
+            self.fail('User must be valid!')
+
+    def _assert_user_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.user.full_clean()
--- microblogs/views.py
+++ microblogs/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .forms import SignUpForm
 
 def home(request):
     response = render(request, 'home.html')
+    return response
+
+
+def sign_up(request):
+    form=SignUpForm()
+    response = render(request, 'sign_up.html', {'form': form})
     return response
